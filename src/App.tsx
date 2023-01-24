c02e1c2e2f17d5079e63d488b3be637cb6508191
Hatus Níwman <hatusn@gmail.com>
2023-01-23 21:41:08 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './global.css';
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div className={styles.wrapper}>
+      <h1>Ola mundo</h1>
+    </div>
+  );
+}
+
+export default App;
